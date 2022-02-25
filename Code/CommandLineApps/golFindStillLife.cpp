8c154a9704f6800450a09fa40311c6e4de9110a7
IvyZayn <kakapon713@outlook.com>
2022-02-25 21:03:07 +0000

    Add the application of Find Still Lifes.

--- Code/CommandLineApps/golFindStillLife.cpp
+++ Code/CommandLineApps/golFindStillLife.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
+#include <utility>
 #include <stdlib.h>
+#include <unistd.h>
 #include "golBasicTypes.h"
 #include "golMyFunctions.h"
 
@@ -10,10 +13,7 @@ static void show_usage(std::string name)
     std::cerr << "Usage: " << name << " <arguments(s)> SOURCES" << "\n"
               << "Get usage (optional):\n"
               << "\t-h,--help\t\tShow this help message\n"
-              << "Run by specifying an input file:\n"
-              << "\tf,filepath FILEPATH\tSpecify the path of input txt file\n"
-              << "\titer,iteration ITERATION_NUM\tSpecify the iteration number of Game of Life\n"
-              << "Run by starting with random cell contents:\n"
+              << "Find Sill Lifes by starting with random cell contents:\n"
               << "\tr,row ROW\tSpecify the row number of the grid\n"
               << "\tc,column COLUMN\tSpecify the column number of the grid\n"
               << "\ta,alive ALIVE_NUM\tSpecify the number of initial alive cells\n"
@@ -24,64 +24,17 @@ static void show_usage(std::string name)
 
 int main(int argc, char* argv[])
 {
-    if (argc < 5) 
+    if (argc < 8) 
     {
         show_usage(argv[0]);
         return 1;
     }
-    else if (argc == 5)
-    {    
-        std::string filepath="";
-        int iter = 0;
-        for (int i = 1; i < argc; ++i) 
-        {
-            std::string arg = argv[i];
-            if ((arg == "-h") || (arg == "--help")) 
-            {
-                show_usage(argv[0]);
-                return 0;
-            } 
-            else if ((arg == "f") || (arg == "filepath")) 
-            {
-                if (i + 1 < argc) 
-                {   
-                    filepath = argv[++i];
-                } 
-                else 
-                { 
-                    show_usage(argv[0]);
-                    return 0;
-                }
-            }
-            else if ((arg == "iter") || (arg == "iteration")) 
-            {
-                if (i + 1 < argc) 
-                {   
-                    iter = atoi(argv[++i]);
-                } 
-                else 
-                { 
-                    std::cerr << "iter requires one iteration number." << std::endl;
-                    return 1;
-                }
-            }
-        }
-        gol::Status s(filepath);
-        gol::GameofLife gof(s);
-        gof.PrintGrid();
-        for(int i=0; i<iter; i++)
-        {
-            gof.TakeStep();
-            gof.PrintGrid();
-        }
-        return 1;
-    }
-    else if (argc == 9)
+    else if ((argc == 8)||(argc == 9))
     {   
         int row = 0;
         int col = 0;
         int alive = 0;
-        int iter_ran = 0;
+        int iter = 0;
         for (int i = 1; i < argc; ++i)
         {
             std::string arg = argv[i];
@@ -125,7 +78,7 @@ int main(int argc, char* argv[])
             {
                 if (i + 1 < argc) 
                 {   
-                    iter_ran = atoi(argv[++i]);
+                    iter = atoi(argv[++i]);
                 } 
                 else 
                 { 
@@ -136,13 +89,68 @@ int main(int argc, char* argv[])
 
         }
         
-        gol::Status s(row,col,alive);
-        gol::GameofLife gof(s);
-        gof.PrintGrid();
-        for(int i=0; i<iter_ran; i++)
+        int rand_cnt = 0;
+        int rand_num = 500;
+        std::vector<gol::Status> rand_grid = {};
+        std::cout << "I'm finding..." << "\n";
+        while(rand_cnt < rand_num)
+        {   
+            gol::Status status(row,col,alive);
+
+            if(rand_grid.size() == 0)
+            {
+                rand_grid.push_back(status);
+            }
+            else
+            {
+                if(status.m_grid != rand_grid[rand_grid.size()-1].m_grid)
+                {
+                    rand_grid.push_back(status);
+                }
+            }
+            ++rand_cnt;
+            sleep(1);
+        }
+
+        std::cout << "Number of searched random grids: " << rand_grid.size() << "\n";
+        std::vector<gol::Status> still_life = {};
+        for (auto status:rand_grid)
+        {   
+            int still_iter = 0;
+            std::vector<std::vector<int>> curr_status = {};
+            std::vector<std::vector<int>> next_status = {};
+            gol::GameofLife gof(status);
+            
+            for(int i=0; i<iter; i++)
+            {
+                curr_status = gof.m_currstatus.m_grid;
+                gof.TakeStep();
+                next_status = gof.m_currstatus.m_grid;
+                if(curr_status == next_status)
+                {
+                    ++still_iter;
+                }
+            }
+
+            if (still_iter == iter)
+            {
+                still_life.push_back(status);
+            }
+        }
+
+        std::cout << "Number of found Still lifes: " <<  still_life.size() << "\n";
+
+        if(still_life.size() == 0)
         {
-            gof.TakeStep();
-            gof.PrintGrid();
+            std::cout << "Cannot find still lifes. " << std::endl;
+        }
+        else
+        {
+            std::cout << "Found Still lifes: " << "\n";
+            for (auto status:still_life)
+            {
+                status.StatusPrint();
+            }
         }
     }
     else
